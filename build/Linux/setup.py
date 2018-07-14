0a51783eb4f11528b91c3dff32c31421be28632d
skl.progs <skl.progs@gmail.com>
2018-07-14 11:25:06 +0300

    Build scripts

---
+++ build/Linux/setup.py
@@ -0,0 +1,21 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict (packages = []
+                    ,includes = ["re"]
+                    ,excludes = []
+                    )
+
+executables = [Executable ('shared.py'
+                          ,base       = 'Console'
+                          ,targetName = 'shared'
+                          )
+              ]
+
+setup (name        = 'shared'
+      ,version     = '1'
+      ,description = 'Shared files for all my Python projects'
+      ,options     = dict(build_exe=buildOptions)
+      ,executables = executables
+      )
---
+++ build/Linux/update_here.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Do not use "verbose" in order to spot errors easily
+
+mkdir -p ./resources/locale/ru/LC_MESSAGES
+
+# Copy shared resources
+cp -u /usr/local/bin/shared/resources/{error,info,question,warning}.gif ./resources/
+
+# Copy other shared resources
+cp -u /usr/local/bin/shared/resources/locale/ru/LC_MESSAGES/shared.mo ./resources/locale/ru/LC_MESSAGES/
+
+# Copy shared Python files
+cp -u /usr/local/bin/shared/src/{gettext_windows,regexp,shared,sharedGUI}.py .
+
+# (Linux-only) Copy build scripts
+cp -u /usr/local/bin/shared/build/Linux/{build.sh,clean_up.sh,setup.py} .
+
+ls .
