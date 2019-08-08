d0e9e33611979880404fb3e0426dbb2010cdb50f
skl.progs <skl.progs@gmail.com>
2019-08-08 11:55:38 +0300

    Add Left, Right bindings for ListBox

--- src/shared.py
+++ src/shared.py
@@ -2558,11 +2558,11 @@ class ListBox:
                  )
         if not self.Multiple:
             com.bind (obj      = self.gui
-                     ,bindings = '<Up>'
+                     ,bindings = ('<Up>','<Left>')
                      ,action   = self.move_up
                      )
             com.bind (obj      = self.gui
-                     ,bindings = '<Down>'
+                     ,bindings = ('<Down>','<Right>')
                      ,action   = self.move_down
                      )
 
