674b595602d25c8a78eb058ab1652f067de78c64
skl.progs <skl.progs@gmail.com>
2023-03-21 00:54:47 +0300

    web: Improve conditioning

--- src/web.py
+++ src/web.py
@@ -15,18 +15,18 @@ class WebPage:
     
     def make_pretty(self):
         f = '[shared] web.WebPage.make_pretty'
-        if self.code:
-            try:
-                bytes_ = lxml.etree.tostring (lxml.etree.XML(self.code)
-                                             ,pretty_print = True
-                                             )
-                result = bytes_.decode('utf-8')
-                return html.unescape(result)
-            except Exception as e:
-                mes = _('Third-party module has failed!\n\nDetails: {}')
-                mes = mes.format(e)
-                sh.objs.get_mes(f,mes).show_error()
-                return self.code
-        else:
+        if not self.code:
             sh.com.rep_empty(f)
+            return ''
+        try:
+            bytes_ = lxml.etree.tostring (lxml.etree.XML(self.code)
+                                         ,pretty_print = True
+                                         )
+            result = bytes_.decode('utf-8')
+            return html.unescape(result)
+        except Exception as e:
+            mes = _('Third-party module has failed!\n\nDetails: {}')
+            mes = mes.format(e)
+            sh.objs.get_mes(f,mes).show_error()
+            return self.code
         return ''
