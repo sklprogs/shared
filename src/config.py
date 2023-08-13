33bb2b7ec5eec3efbd2fd015454a62a1ac7edb05
skl.progs <skl.progs@gmail.com>
2023-08-14 00:40:40 +0300

    Rework config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import os
+import copy
 import json
 import jsonschema
 
@@ -8,127 +10,354 @@ from skl_shared_qt.localize import _
 import skl_shared_qt.shared as sh
 
 
+''' We need to load the default config anyway since the local config can be
+    outdated and may not comprise new keys. We cannot just check the local
+    config against the default schema since the latter checks the presence of
+    default keys and the program will force default configuration each time
+    new keys are added to the default config instead of just updating the local
+    config. Checking the presence of default keys is also necessary since we
+    will have to try-except all config values otherwise.
+'''
+
+
 class Json:
     
-    def __init__(self, file, schema={}, invalid_mes=''):
-        self.Success = True
+    def __init__(self, file, Silent=False):
         self.code = ''
         self.json = {}
         self.file = file
-        self.schema = schema
-        self.invalid_mes = invalid_mes
+        self.Silent = Silent
+        ''' Since absent config is allowed, this class fails only in case of
+            JSON exceptions. Schema is allowed to fail since local config can
+            be outdated.
+        '''
+        self.Success = True
     
-    def validate(self):
+    def validate(self, schema):
         f = '[SharedQt] config.Json.validate'
-        if not self.Success:
-            sh.com.cancel(f)
+        if not self.json or not schema:
+            sh.com.rep_empty(f)
             return
         # Setting empty schema passes validation
         try:
-            jsonschema.validate(self.json, self.schema)
+            jsonschema.validate(self.json, schema)
+            return True
         except jsonschema.exceptions.ValidationError as e:
-            self.Success = False
-            if self.invalid_mes:
-                mes = self.invalid_mes + '\n\n' + _('Details:\n{}').format(e)
-            else:
-                mes = _('Configuration file "{}" is invalid!\n\nDetails:\n{}')
-                mes = mes.format(self.file, e)
-            sh.objs.get_mes(f, mes).show_error()
+            mes = _('Configuration file "{}" is invalid!\n\nDetails:\n{}')
+            mes = mes.format(self.file, e)
+            sh.objs.get_mes(f, mes, self.Silent).show_error()
     
     def load(self):
         f = '[SharedQt] config.Json.load'
-        if not self.Success:
-            sh.com.cancel(f)
-            return
         if not self.file:
-            self.Success = False
             sh.com.rep_empty(f)
+            return
+        if not os.path.exists(self.file):
+            sh.com.rep_lazy(f)
             return
         ''' Show the full path in case of not finding the file to make
             debugging easier.
         '''
         self.file = sh.Path(self.file).get_absolute()
-        self.code = sh.ReadTextFile(self.file).get()
-        if not self.code:
-            self.Success = False
+        code = sh.ReadTextFile(self.file).get()
+        if not code:
             sh.com.rep_out(f)
             return
-    
-    def set(self):
-        f = '[SharedQt] config.Json.set'
-        if not self.Success:
-            sh.com.cancel(f)
-            return
         try:
-            self.json = json.loads(self.code)
+            self.json = json.loads(code)
         except Exception as e:
             self.Success = False
             sh.com.rep_third_party(f, e)
+            return
+        return True
     
-    def save(self, body):
+    def dump(self):
+        f = '[SharedQt] config.Json.dump'
+        if not self.json:
+            sh.com.rep_empty(f)
+            return
+        try:
+            return json.dumps(self.json, ensure_ascii=False, indent=4)
+        except Exception as e:
+            sh.com.rep_third_party(f, e)
+    
+    def save(self, obj):
         f = '[SharedQt] config.Json.save'
+        self.json = obj
+        code = self.dump()
+        if not code:
+            sh.com.rep_empty(f)
+            return
+        return sh.WriteTextFile(self.file, True).write(code)
+
+
+
+class Schema:
+    
+    def __init__(self, file):
+        self.Success = True
+        self.file = file
+        self.iconfig = Json(self.file)
+    
+    def get(self):
+        f = '[SharedQt] config.Schema.get'
         if not self.Success:
             sh.com.cancel(f)
-            return
-        try:
-            code = json.dumps(body, ensure_ascii=False, indent=4)
-        except Exception as e:
-            self.Success = False
-            sh.com.rep_third_party(f, e)
-            return
-        self.Success = sh.WriteTextFile(self.file, True).write(code)
+            return {}
+        return self.iconfig.json
+    
+    def dump(self):
+        f = '[SharedQt] config.Schema.dump'
+        if not self.Success:
+            sh.com.cancel(f)
+            return ''
+        code = self.iconfig.dump()
+        if not code:
+            sh.com.rep_empty(f)
+            return ''
+        return code
+    
+    def load(self):
+        f = '[SharedQt] config.Schema.load'
+        if not self.Success:
+            sh.com.cancel(f)
+            return {}
+        self.Success = self.iconfig.load()
     
     def run(self):
         self.load()
-        self.set()
+
+
+
+class Default:
+    
+    def __init__(self, file, schema):
+        self.Success = True
+        self.file = file
+        self.iconfig = Json(self.file)
+        self.schema = schema
+    
+    def get(self):
+        f = '[SharedQt] config.Default.get'
+        if not self.Success:
+            sh.com.cancel(f)
+            return {}
+        return self.iconfig.json
+    
+    def dump(self):
+        f = '[SharedQt] config.Default.dump'
+        if not self.Success:
+            sh.com.cancel(f)
+            return ''
+        code = self.iconfig.dump()
+        if not code:
+            sh.com.rep_empty(f)
+            return ''
+        return code
+    
+    def load(self):
+        f = '[SharedQt] config.Default.load'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.Success = self.iconfig.load()
+    
+    def get_version(self):
+        f = '[SharedQt] config.Default.get_version'
+        if not self.Success:
+            sh.com.cancel(f)
+            return 0
+        try:
+            return self.iconfig.json['config']['min_version']
+        except KeyError:
+            self.Success = False
+            return 0
+    
+    def validate(self):
+        f = '[SharedQt] config.Default.validate'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.Success = self.iconfig.validate(self.schema)
+    
+    def run(self):
+        self.load()
         self.validate()
-        return self.json
+        self.get_version()
+
+
+
+class Local:
+    
+    def __init__(self, file, min_version):
+        self.Success = True
+        self.file = file
+        self.iconfig = Json(self.file)
+        self.min_version = min_version
+    
+    def get(self):
+        f = '[SharedQt] config.Local.get'
+        if not self.Success:
+            sh.com.cancel(f)
+            return {}
+        return self.iconfig.json
+    
+    def dump(self):
+        f = '[SharedQt] config.Local.dump'
+        if not self.Success:
+            sh.com.cancel(f)
+            return '{}'
+        code = self.iconfig.dump()
+        if not code:
+            sh.com.rep_empty(f)
+            return '{}'
+        return code
+    
+    def load(self):
+        f = '[SharedQt] config.Local.load'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.iconfig.load()
+        # Absent file is allowed, but not JSON exceptions
+        self.Success = self.iconfig.Success
+    
+    def get_version(self):
+        try:
+            return self.iconfig.json['config']['min_version']
+        except KeyError:
+            self.Success = False
+            return 0
+    
+    def check_version(self):
+        f = '[SharedQt] config.Local.check_version'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        ''' We need to check the existence and validity of the config version
+            key. This can also be done by duplicating a schema for the local
+            config without checking key existence (but will require more effort
+            in maintaining schemas).
+        '''
+        if not 'config' in self.iconfig.json:
+            self.Success = False
+            mes = _('Configuration file "{}" does not have key "{}"!')
+            mes = mes.format(self.iconfig.file, 'config')
+            sh.objs.get_mes(f, mes, True).show_warning()
+            return
+        if not isinstance(self.iconfig.json['config'], dict):
+            self.Success = False
+            mes = _('Configuration file "{}": key "{}" has a wrong type!')
+            mes = mes.format(self.iconfig.file)
+            sh.objs.get_mes(f, mes, True).show_warning()
+            return
+        if not 'min_version' in self.iconfig.json['config']:
+            self.Success = False
+            mes = _('Configuration file "{}" does not have key "{}"!')
+            mes = mes.format(self.iconfig.file, "['config']['min_version']")
+            sh.objs.get_mes(f, mes, True).show_warning()
+            return
+        if not isinstance(self.iconfig.json['config']['min_version'], int):
+            self.Success = False
+            mes = _('Configuration file "{}": key "{}" has a wrong type!')
+            mes = mes.format(self.iconfig.file, "['config']['min_version']")
+            sh.objs.get_mes(f, mes, True).show_warning()
+            return
+        if self.iconfig.json['config']['min_version'] != self.min_version:
+            self.Success = False
+            mes = _('Wrong version {}, expected {}!')
+            mes = mes.format (self.iconfig.json['config']['min_version']
+                             ,self.min_version
+                             )
+            sh.objs.get_mes(f, mes, True).show_warning()
+    
+    def save(self, obj):
+        # Should run even if Success == False
+        return self.iconfig.save(obj)
+    
+    def run(self):
+        self.load()
+        self.check_version()
 
 
 
 class Config:
     
-    def __init__(self, file, pschema, invalid_mes=''):
+    def __init__(self, default, schema, local):
+        self.set_values()
+        self.default = default
+        self.schema = schema
+        self.local = local
+    
+    def set_values(self):
         self.Success = True
-        self.ischema = None
-        self.iconfig = None
-        self.file = file
-        self.pschema = pschema
-        self.invalid_mes = invalid_mes
+        self.schema = ''
+        self.default = ''
+        self.local = ''
+        self.new = {}
     
-    def load_schema(self):
-        f = '[SharedQt] config.Config.load_schema'
+    def _copy(self):
+        self.new = copy.deepcopy(self.idefault.get())
+    
+    def update(self):
+        f = '[SharedQt] config.Config.update'
         if not self.Success:
             sh.com.cancel(f)
             return
-        self.ischema = Json(self.pschema)
-        # Empty schema is considered valid in 'Json', but not here
-        if not self.ischema.run():
-            self.Success = False
-    
-    def load_config(self):
-        f = '[SharedQt] config.Config.load_config'
-        if not self.Success:
-            sh.com.cancel(f)
-            return
-        self.iconfig = Json(self.file, self.ischema.schema, self.invalid_mes)
-        return self.iconfig.run()
+        if self.ilocal.Success:
+            mes = _('Update default configuration')
+            self.new = self.idefault.get() | self.ilocal.get()
+        else:
+            mes = _('Use default configuration')
+            self._copy()
+        sh.objs.get_mes(f, mes, True).show_info()
     
     def load(self):
-        self.load_schema()
-        return self.load_config()
+        f = '[SharedQt] config.Config.load'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.ischema = Schema(self.schema)
+        self.ischema.run()
+        self.idefault = Default(self.default, self.ischema.get())
+        self.idefault.run()
+        self.ilocal = Local(self.local, self.idefault.get_version())
+        self.ilocal.run()
+        self.Success = self.ischema.Success and self.idefault.Success
     
-    def save(self, body):
+    def get(self):
+        f = '[SharedQt] config.Config.get'
+        if not self.Success:
+            sh.com.cancel(f)
+            return {}
+        return self.new
+    
+    def dump(self):
+        f = '[SharedQt] config.Config.dump'
+        if not self.Success:
+            sh.com.cancel(f)
+            return '{}'
+        try:
+            return json.dumps(self.new, ensure_ascii=False, indent=4)
+        except Exception as e:
+            sh.com.rep_third_party(f, e)
+        return '{}'
+    
+    def quit(self):
+        self.revert_types()
+        self.save()
+    
+    def save(self):
         f = '[SharedQt] config.Config.save'
         if not self.Success:
             sh.com.cancel(f)
             return
-        ''' 'Config' is designed to be a higher level than 'Json', so we allow
-            empty contents in 'Json' but not here.
-        '''
-        if not body:
-            self.Success = False
-            sh.com.rep_empty(f)
+        # Do not forget to revert unsupported types back to strings first
+        if self.dump() == self.ilocal.dump():
+            sh.com.rep_lazy(f)
             return
-        self.iconfig.save()
-        self.Success = self.iconfig.Success
+        self.Success = self.ilocal.save(self.new)
+    
+    def run(self):
+        self.load()
+        self.update()
