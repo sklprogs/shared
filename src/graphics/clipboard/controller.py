81b26061d42b62635940b04f1393384d951310c7
skl.progs <skl.progs@gmail.com>
2024-11-09 03:22:22 +0300

    graphics.clipboard: Support graphical warnings

--- src/graphics/clipboard/controller.py
+++ src/graphics/clipboard/controller.py
@@ -7,19 +7,19 @@ import skl_shared_qt.graphics.clipboard.gui as gi
 
 class Clipboard:
 
-    def __init__(self, Silent=False):
-        self.Silent = Silent
+    def __init__(self, Graphical=True):
+        self.Graphical = Graphical
         self.gui = gi.Clipboard()
 
     def copy(self, text, CopyEmpty=True):
         f = '[SharedQt] graphics.clipboard.controller.Clipboard.copy'
         if not (text or CopyEmpty):
-            ms.rep.empty(f)
+            ms.rep.empty(f, self.Graphical)
             return
         try:
             self.gui.copy(text)
         except Exception as e:
-            ms.rep.failed(f, e, self.Silent)
+            ms.rep.failed(f, e, self.Graphical)
 
     def paste(self):
         f = '[SharedQt] graphics.clipboard.controller.Clipboard.paste'
@@ -27,9 +27,6 @@ class Clipboard:
             text = self.gui.paste()
         except Exception as e:
             text = ''
-            ms.rep.failed(f, e, self.Silent)
+            ms.rep.failed(f, e, self.Graphical)
         # Further possible actions: strip, delete double line breaks
         return text
-
-
-CLIPBOARD = Clipboard()
